f84bccd04e6f41f50e732b657957c8a8c1875d1d
ho_quoc_anh@allgrow-labo.jp <ho_quoc_anh@allgrow-labo.jp>
2021-06-23 13:34:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React JS Demo deploy Heroku
+          React JS Allgor Labo Demo
         </p>
         <a
           className="App-link"
